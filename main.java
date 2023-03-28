6bff3b2c87006446cd50bbe8c2f87c46f762a8f1
Sreedeepta <78677605+sreedeepta@users.noreply.github.com>
2023-03-28 10:42:14 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+class Program6
+{
+    public void calculate(int x, int y, int z)
+    {
+        int s;
+        double a;
+        s=x+y+z; 
+        a=s/3.0; 
+        System.out.println("Sum = "+s); 
+        System.out.println("Average = "+a);
+    }
+}
